7d873b9de6735a2f790a9a0fb4ac8d195973b6ba
yaroyazeed <53499828+yaroyazeed@users.noreply.github.com>
2021-08-25 18:09:38 +0100

    Resolved Api Key
    Resolved Api Key

--- app/src/main/java/com/example/currencyconverter/helper/EndPoints.kt
+++ app/src/main/java/com/example/currencyconverter/helper/EndPoints.kt
@@ -9,7 +9,7 @@ class EndPoints {
 
         //API KEY - Go to geo currency converter website, obtain an                                       API key and paste it between " "
 
-        const val API_KEY = " "
+        const val API_KEY = "8d79b3b0dbce77d1605d9a939d64f817a7eab762"
 
         //COVERT URL
         const val CONVERT_URL = "convert"
