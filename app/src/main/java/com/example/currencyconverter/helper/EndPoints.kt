d3deaac6b2e30d52731dfd64db1a6bb542be4cbd
yaroyazeed <53499828+yaroyazeed@users.noreply.github.com>
2021-08-25 18:10:33 +0100

    Update EndPoints.kt

--- app/src/main/java/com/example/currencyconverter/helper/EndPoints.kt
+++ app/src/main/java/com/example/currencyconverter/helper/EndPoints.kt
@@ -7,8 +7,6 @@ class EndPoints {
         //Base URL
         const val BASE_URL = "https://api.getgeoapi.com/api/v2/currency/"
 
-        //API KEY - Go to geo currency converter website, obtain an                                       API key and paste it between " "
-
         const val API_KEY = "8d79b3b0dbce77d1605d9a939d64f817a7eab762"
 
         //COVERT URL
